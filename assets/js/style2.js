4585b3f13d5e6700b52d08e57f6a4c07fa3724cb
milagros acosta <milyarias91@gmail.com>
2022-02-03 22:21:42 -0300

    first commit

---
+++ assets/js/style2.js
@@ -0,0 +1,12 @@
+const number1 = prompt("Ingrese un número:");
+const number2 = prompt("Ingrese un número diferente al anterior:");
+//console.log(typeoff Number numero1);
+
+//number1 y number2 deben ser distintos entre sí y distintos de cero!
+if (number1 --- number2){
+    console.log("Los números son iguales");
+}
+
+if (number1 != number2){
+    console.log("Los números son distintos");
+}
